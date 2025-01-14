16442bf801ba31be1f6e639e1d245391c829af46
localvos <dev@dvos.me>
2025-01-14 13:03:30 +0100

    start one saving fails

--- handlers/fails_handlers.go
+++ handlers/fails_handlers.go
@@ -1,1 +1,19 @@
 package handlers
+
+import (
+	"project/models"
+	"project/repository"
+)
+
+type FailsController struct {
+	FailRepoContr repository.FailedRepoController
+	UserRepoContr repository.UserRepoController
+}
+
+func (c *FailsController) GetFails(steamId string) ([]models.Fail, error) {
+	fails, err := c.FailRepoContr.GetFailsForUser(steamId)
+	if err != nil {
+		return nil, err
+	}
+	return fails, nil
+}
--- repository/failed_repo.go
+++ repository/failed_repo.go
@@ -1,9 +1,31 @@
 package repository
 
-import "project/models"
+import (
+	"project/config"
+	"project/models"
+)
 
 type FailedRepoController struct {
 	DbContr *DatabaseController
+}
+
+func (c *FailedRepoController) GetFailsForUser(steamId string) ([]models.Fail, error) {
+	query := "SELECT * FROM failed_games WHERE steam_id = ?"
+	rows, err := c.DbContr.FetchRowsWithClose(query, steamId)
+	if err != nil {
+		config.HandleError("failed to parse GetFailsForUser", err)
+	}
+	defer closeRows(rows)
+	var fails []models.Fail
+	if rows.Next() {
+		fail, err := models.MapFail(rows)
+		if err != nil {
+			config.HandleError("failed to map GetFailsForUser", err)
+			return nil, err
+		}
+		fails = append(fails, fail)
+	}
+	return fails, nil
 }
 
 func (c *FailedRepoController) CreateFailed(fail models.Fail) error {
