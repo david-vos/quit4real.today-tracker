e00e213471952043c5daddbf5445cfa89e0650ab
localvos <dev@dvos.me>
2025-01-27 00:37:20 +0100

    Ip addresses for prod

--- src/endpoint/init.go
+++ src/endpoint/init.go
@@ -1,19 +1,58 @@
 package endpoint
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"net"
 	"net/http"
 	"quit4real.today/logger"
 )
 
 type Endpoints struct {
-	// This router pointer is global, it shouldn't be. I have yet to think of a clean way to instantiate every
-	// endpoint on the same router while still keeping all the Endpoints separated from each other :thinking:
-	// I think mux has a router.sub-route but this will have to work for now...
 	Router               *mux.Router
 	UserEndpoint         *UserEndpoint
 	FailEndpoint         *FailEndpoint
 	SubscriptionEndpoint *SubscriptionEndpoint
+}
+
+// getLocalIPs returns a list of local IPv4 addresses
+func getLocalIPs() []string {
+	var ips []string
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		logger.Fail("Error fetching network interfaces: " + err.Error())
+		return ips
+	}
+
+	for _, iface := range interfaces {
+		// Skip interfaces that are down or loopback interfaces
+		if iface.Flags&net.FlagUp == 0 || iface.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+
+		addrs, err := iface.Addrs()
+		if err != nil {
+			logger.Fail("Error fetching addresses for interface " + iface.Name + ": " + err.Error())
+			continue
+		}
+
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			// Only include IPv4 addresses
+			if ip != nil && ip.To4() != nil {
+				ips = append(ips, ip.String())
+			}
+		}
+	}
+
+	return ips
 }
 
 func Init(endpoints *Endpoints) {
@@ -23,8 +62,25 @@ func Init(endpoints *Endpoints) {
 	endpoints.SubscriptionEndpoint.Subscription()
 
 	go func() {
-		logger.Info("Starting the server. If you do not receive and error it has started successfully")
-		if err := http.ListenAndServe(":8080", endpoints.Router); err != nil {
+		// Bind to 0.0.0.0:8080
+		listener, err := net.Listen("tcp", "0.0.0.0:8080")
+		if err != nil {
+			logger.Fail("Failed to create listener: " + err.Error())
+			return
+		}
+
+		// Log the available IPs with the port
+		ips := getLocalIPs()
+		if len(ips) > 0 {
+			for _, ip := range ips {
+				logger.Info(fmt.Sprintf("Server is accessible at http://%s:8080", ip))
+			}
+		} else {
+			logger.Debug("No active network interfaces found. Server might not be reachable.")
+		}
+
+		// Start the server using the listener
+		if err := http.Serve(listener, endpoints.Router); err != nil {
 			logger.Fail("Failed to start server: " + err.Error())
 		}
 	}()
